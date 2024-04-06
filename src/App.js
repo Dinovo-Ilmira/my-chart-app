423a3de18097feb5e7ec7a893e550273a88ba95f
Dinovo-Ilmira <dinovo.ilmira@gmail.com>
2024-04-05 21:06:02 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>График типа Pie</h1>
+      <Chart
+        width={'100%'}
+        height={'400px'}
+        chartType="PieChart"
+        loader={<div>Loading Chart</div>}
+        data={[
+          ['Задача', 'Часов в день'],
+          ['Работа', 11],
+          ['Еда', 2],
+          ['Дорога', 2],
+          ['Телевизор', 2],
+          ['Сон', 7],
+        ]}
+        options={{
+          title: 'Мои ежедневные занятия',
+        }}
+        rootProps={{ 'data-testid': '1' }}
+      />
     </div>
   );
 }
